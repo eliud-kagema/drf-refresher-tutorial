2e2dea9f13d81194315df1020c4d584a2d0136f9
Eliud Kagema <eliud.k.maina@gmail.com>
2021-10-27 15:59:18 +0300

    setting up polls models

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls', namespace='blog')),
+    path('polls/', include('polls.urls')),
 
 ]
