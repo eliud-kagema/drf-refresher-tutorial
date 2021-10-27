dfba432dd82ce70b6c54318722298f4a6834f5c6
Eliud Kagema <eliud.k.maina@gmail.com>
2021-10-27 16:14:09 +0300

    Start Chapter 5

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -15,32 +15,87 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Poll',
+            name="Poll",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('question', models.CharField(max_length=100)),
-                ('pub_date', models.DateTimeField(auto_now=True)),
-                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("question", models.CharField(max_length=100)),
+                ("pub_date", models.DateTimeField(auto_now=True)),
+                (
+                    "created_by",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Choice',
+            name="Choice",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('choice_text', models.CharField(max_length=100)),
-                ('poll', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='choices', to='polls.poll')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("choice_text", models.CharField(max_length=100)),
+                (
+                    "poll",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="choices",
+                        to="polls.poll",
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Vote',
+            name="Vote",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('choice', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='votes', to='polls.choice')),
-                ('poll', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.poll')),
-                ('voted_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "choice",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="votes",
+                        to="polls.choice",
+                    ),
+                ),
+                (
+                    "poll",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="polls.poll"
+                    ),
+                ),
+                (
+                    "voted_by",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
             options={
-                'unique_together': {('poll', 'voted_by')},
+                "unique_together": {("poll", "voted_by")},
             },
         ),
     ]
--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,9 @@ from .models import Poll
 def polls_list(request):
     MAX_OBJECTS = 20
     polls = Poll.objects.all()[:MAX_OBJECTS]
-    data = {"results": list(polls.values("question", "created_by__username", "pub_date"))}
+    data = {
+        "results": list(polls.values("question", "created_by__username", "pub_date"))
+    }
     return JsonResponse(data)
 
 
@@ -14,9 +16,10 @@ def polls_detail(request, pk):
     poll = get_object_or_404(Poll, pk=pk)
     data = {
         "results": {
-        "question": poll.question,
-        "created_by": poll.created_by.username,
-        "pub_date": poll.pub_date
-    }}
-    
+            "question": poll.question,
+            "created_by": poll.created_by.username,
+            "pub_date": poll.pub_date,
+        }
+    }
+
     return JsonResponse(data)
